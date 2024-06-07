e0085fa9496b00949e191b173bd07f19f1f7ff58
Eljohnz <eljohnzzz8@gmail.com>
2024-06-07 23:06:30 +0800

    TRY

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let userInput = []
 const saveEl = document.querySelector("#main__body--button")
 const inputEl = document.querySelector("#main__body--input")
+const listEl = document.querySelector("#list")
 
 saveEl.addEventListener ("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    listEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
